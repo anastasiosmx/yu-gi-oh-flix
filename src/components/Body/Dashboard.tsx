80b2f80c1aa749d030171c5d913647adb50a7225
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-11-23 21:07:46 +0200

    Refactor (Dashboard): Changed boilerplate code with component call for cardList & altered card contianer size to fit image standard

--- src/components/Body/Dashboard.tsx
+++ src/components/Body/Dashboard.tsx
@@ -1,99 +1,13 @@
+import { CardList } from './Card/CardList';
 import styles from './Dashboard.module.css';
+import useGetLastPage from '../../hooks/getLastPage';
 
 export const Dashboard = () => {
+    const lastPageCounter = useGetLastPage();
     return(
         <>
             <div className={styles.DashboardWrapper}>
-                <div className={styles.DashboardWrapper_card}>
-                    test
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
-                <div className={styles.DashboardWrapper_card}>
-                    test5
-                </div>
+                <CardList />
             </div>
         </>
     );
