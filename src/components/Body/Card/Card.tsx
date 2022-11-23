a6c3ec1c7c88796c475d12d12333d9b36b9eabbd
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-11-23 21:08:32 +0200

    Feat (Card): Initliazed Card component & added style

--- src/components/Body/Card/Card.tsx
+++ src/components/Body/Card/Card.tsx
@@ -1,9 +1,13 @@
 import styles from './Card.module.css';
 
-export const Card = () => {
+export const Card = ({ name, id }: any) => {
     return(
         <>
-            hello
+            <div className={styles.CardWrapper}>
+                <div className={styles.CardWrapper_cardImage}>
+                    <img src={`cards_imgs/${id}.jpg`} alt={name} className={styles.CardWrapper_cardImage_size}/>
+                </div>
+            </div>
         </>
     );
 }
