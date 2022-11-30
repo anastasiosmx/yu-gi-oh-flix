9e48c2e7cdee78e0dd37664e9e14e7861ec5760a
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-11-30 18:20:53 +0200

    Feat (Header): Added state for search field

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,26 @@
 import logo from '../../images/yu-gi-oh-logo.png';
 import styles from './Header.module.css';
+import { useState } from 'react';
 
 export const Header = () => {
+    const [searchParameter, setSearchParameter] = useState('');
+
+    const handleSearch = (event: any) =>{
+        setSearchParameter(event.target.value);
+    }
+
     return(
         <>
             <div className={styles.HeaderWrapper}>
                 <img className={styles.HeaderWrapper_logoImg} src={logo} alt="logo" />
-                <input type="text" className={styles.HeaderWrapper_searchBar} placeholder='Search for any card using their name... e.g. "Crystal Tree"'/>
+                <input 
+                    type="text"
+                    name='search'
+                    className={styles.HeaderWrapper_searchBar}
+                    placeholder='Search for any card using their name... e.g. "Crystal Tree"'
+                    onChange={event => handleSearch(event)}
+                    value={searchParameter}
+                />
             </div>
         </>
     );
