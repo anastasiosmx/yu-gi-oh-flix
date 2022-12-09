8229d9c320c21a30c84ea3ad25bb5fd9f69b928d
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-12-09 06:39:58 +0200

    Feat (Header): Added radio buttons for filters

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,12 +15,22 @@ export const Header = (props: any) => {
             <div className={styles.HeaderWrapper}>
                 <img className={styles.HeaderWrapper_logoImg} src={logo} alt="logo" />
                 <input 
-                    type="text"
+                    type='text'
                     name='search'
                     className={styles.HeaderWrapper_searchBar}
                     placeholder='Search for any set of cards or individual cards e.g. "Crystal Tree"'
                     onKeyDown={event => handleSearch(event)}
                 />
+                <div className={styles.HeaderWrapper_filters} >
+                    <input type='radio' id='freeSearch' value='free' name='filters' />
+                    <label htmlFor='freeSearch' className={styles.HeaderWrapper_filters__filterItem}>Free</label>
+
+                    <input type='radio' id='name' value='name' name='filters'/>
+                    <label htmlFor='name' className={styles.HeaderWrapper_filters__filterItem}>Name</label>
+
+                    <input type='radio' id='type' value='type' name='filters'/>
+                    <label htmlFor='type' className={styles.HeaderWrapper_filters__filterItem}>Type</label>
+                </div>
             </div>
         </>
     );
