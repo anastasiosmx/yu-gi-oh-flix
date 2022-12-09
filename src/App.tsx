564c4d2dc869fa6b5827f7814261e47ddf9414ee
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-12-09 05:45:09 +0200

    Fix( App): fixed typo in setState method

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Header } from './components/Header/Header';
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [searchParameter, setSearchParameter] = useState('');
-  const [searchFilterKeyword, setsearchFilterKeyword] = useState('&q=');
+  const [searchFilterKeyword, setSearchFilterKeyword] = useState('&q=');
   
   function changePageHandler(pageNumber: number){
     setCurrentPage(pageNumber);
