a8b5963bee42546dc108bcc9317e7aaf05385b09
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-12-13 16:06:54 +0200

    Feat (Card): Added backside contnet

--- src/components/Body/Card/Card.tsx
+++ src/components/Body/Card/Card.tsx
@@ -1,6 +1,6 @@
 import styles from './Card.module.css';
 
-export const Card = ({ name, id }: any) => {
+export const Card = ({ name, id, description, race, type, atk }: any) => {
     return(
         <>
             <div className={styles.CardWrapper}>
@@ -9,7 +9,24 @@ export const Card = ({ name, id }: any) => {
                         <img src={`cards_imgs/${id}.jpg`} alt={name} className={styles.CardWrapper_cardImage_size}/>
                     </div>
                     <div className={styles.CardWrapper_cardImage__back} >
-                        <h1>TEST</h1>
+                        <h3 className={styles.CardWrapper_cardImage__back___name}>
+                            {name}
+                        </h3>
+
+                        <h4 className={styles.CardWrapper_cardImage__back___descriptionHeader}>
+                            Description
+                        </h4>
+                        { description }
+                        
+                        <h4 className={styles.CardWrapper_cardImage__back___descriptionHeader}>
+                            Race
+                        </h4>
+                        { race }
+                        
+                        <h4 className={styles.CardWrapper_cardImage__back___descriptionHeader}>
+                            Type
+                        </h4>
+                        { type }
                     </div>
                 </div>
             </div>
